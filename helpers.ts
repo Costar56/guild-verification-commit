a5c34ef15cae199cbec4f178e8d40118aed00c31
Costar56 <costariko56@outlook.com>
2026-03-08 22:32:25 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T19:31:39.090Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-08T19:32:26.227Z
+Commit: Resolve timezone issues in date handling
