3ab199cb1ea9f14ab702d39ec59ff0299dd614ab
Costar56 <costariko56@outlook.com>
2026-03-08 22:31:13 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T19:31:03.453Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-08T19:31:14.248Z
+Commit: Fix broken links in documentation
