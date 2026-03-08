1cecb313c760fdc032b67e389724728d1e12944d
Costar56 <costariko56@outlook.com>
2026-03-08 22:31:51 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T19:31:30.975Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-08T19:31:51.705Z
+Commit: Resolve null pointer exception in user service
