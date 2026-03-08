6410f99d1ede032539d831bddce960f94574e080
Costar56 <costariko56@outlook.com>
2026-03-08 22:33:20 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T19:33:00.087Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T19:33:20.857Z
+Commit: Fix memory leak in event handlers
