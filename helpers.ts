8f0ca7b7553bb9a6e07464f2991cba81e5273431
Costar56 <costariko56@outlook.com>
2026-03-08 22:33:15 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T19:33:06.966Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-08T19:33:15.910Z
+Commit: Resolve null pointer exception in user service
