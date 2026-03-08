b6cd8754a19380c1cef836eb650cda0d77299f44
Costar56 <costariko56@outlook.com>
2026-03-08 22:33:19 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T19:33:15.910Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-08T19:33:19.791Z
+Commit: Add dark mode support
