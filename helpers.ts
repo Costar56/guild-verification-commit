2a96d663e6a771a2d97c9ca456926b0738aad9dc
Costar56 <costariko56@outlook.com>
2026-03-08 22:33:06 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T19:32:26.227Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-08T19:33:06.966Z
+Commit: Fix memory leak in event handlers
